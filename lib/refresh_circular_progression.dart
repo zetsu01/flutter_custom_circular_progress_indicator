af92ace8bb072b4a24ede1dc992d4961eef4f078
zetsu01 <soviguidivenance@gmail.com>
2021-03-04 03:47:14 +0100

    add example project and build first package version

--- lib/refresh_circular_progression.dart
+++ lib/refresh_circular_progression.dart
@@ -1,7 +1,25 @@
 library refresh_circular_progression;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomCircularProgress {
+  static showIndicator(BuildContext context,
+      {double size = 40, double elevation = 2.0, double strokeWidth = 2}) {
+    return Container(
+      height: size,
+      width: size,
+      margin: EdgeInsets.all(5),
+      child: Material(
+        type: MaterialType.circle,
+        color: Theme.of(context).canvasColor,
+        elevation: elevation,
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: CircularProgressIndicator(
+            strokeWidth: strokeWidth,
+          ),
+        ),
+      ),
+    );
+  }
 }
